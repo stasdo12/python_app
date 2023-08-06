b153d4e36f5c829e13da7ac074bad9907e3678cc
stasdo12 <santeh.vodavdom@gmail.com>
2023-08-06 10:40:52 +0300

    create CRUD web app

--- project/news/models.py
+++ project/news/models.py
@@ -10,6 +10,9 @@ class Articles(models.Model):
     def __str__(self):
         return self.title
 
+    def get_absolute_url(self):
+        return f'/news/{self.id}'
+
     # Table name in admin panel
     class Meta:
         verbose_name = "New"
